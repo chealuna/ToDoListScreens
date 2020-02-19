3703788d003247ad7477ff21a5bdc0926a3c7ba0
Chea <cwright2020@ihs.immaculateheart.org>
2020-02-19 14:02:29 -0800

    Updates

--- app/src/main/java/com/example/helloworld/EditTask.java
+++ app/src/main/java/com/example/helloworld/EditTask.java
@@ -1,46 +1,28 @@
 package com.example.helloworld;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
-
-public class MainActivity extends AppCompatActivity {
-    //1 creating variable
-    Button clickToBegin;
-
+class EditTask {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCretate(savedInstanceState);
+        setContentView(R.layout.screen2);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-            }
-        });
-        //2 get corresponding xml button
-        clickToBegin = findViewById(R.id.button);
-        //3 add event listener
-        clickToBegin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,EditTask.class);
-                startActivity(intent);
             }
         });
     }
@@ -56,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
