3ac3c00ee562756fd1e7dad4034eb09bed94aca5
Chea <cwright2020@ihs.immaculateheart.org>
2020-03-02 12:38:19 -0800

    Updates

--- app/src/main/java/com/example/helloworld/Screen2.java
+++ app/src/main/java/com/example/helloworld/Screen2.java
@@ -4,24 +4,74 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-class Screen2 {
+class Screen2 extends AppCompatActivity {
+    Button specificList;
+    Button checkOff;
+    Button viewPriority;
+    Button editTask;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCretate(savedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.screen2);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+
+        //2 get corresponding xml button
+        specificList = findViewById(R.id.button2);
+        //3 add event listener
+        specificList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(Screen2.this,SpecificList.class);
+                startActivity(intent);
             }
         });
+
+        checkOff = findViewById(R.id.button3);
+        checkOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Screen2.this,CheckOff.class);
+                startActivity(intent);
+            }
+        });
+
+        viewPriority = findViewById(R.id.button7);
+        viewPriority.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Screen2.this,ViewPriority.class);
+                startActivity(intent);
+            }
+        });
+
+        editTask = findViewById(R.id.button4);
+        editTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Screen2.this,EditTask.class);
+                startActivity(intent);
+            }
+        });
+
+        newList = findViewById(R.id.button8);
+        newList.setOnClickListener(new view.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Screen2.this,NewList.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
