c2b923d06af0e51d2cf7e04b8af1b94988f5e2af
ltoal <ltoal@ihs.immaculateheart.org>
2020-05-11 10:55:32 -0700

    Create DatabaseClient

--- app/src/main/java/com/example/helloworld/DatabaseClient.java
+++ app/src/main/java/com/example/helloworld/DatabaseClient.java
@@ -5,5 +5,26 @@ import android.content.Context;
 
 public class DatabaseClient {
 
-    
+    private Context mCtx;
+    private static DatabaseClient mInstance;
+
+    private AppDatabase appDatabase;
+
+    private DatabaseClient(Context mCtx) {
+        this.mCtx = mCtx;
+
+        //creating the app database with Room database builder
+        appDatabase = Room.databaseBuilder(mCtx, AppDatabase.class, "MyToDos").build();
+    }
+
+    public static synchronized DatabaseClient getInstance(Context mCtx) {
+        if (mInstance == null) {
+            mInstance = new DatabaseClient(mCtx);
+        }
+        return mInstance;
+    }
+
+    public AppDatabase getAppDatabase() {
+        return appDatabase;
+    }
 }
